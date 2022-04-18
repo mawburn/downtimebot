23b5d9146672889bc73303dbc242dae23aa823d7
mawburn <mawburn7@gmail.com>
2022-04-18 18:23:49 -0400

    update files

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-export const x = 1
+import { Client, Intents } from 'discord.js';
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'beep') {
+		await interaction.reply('Boop!');
+	}
+});
+
+client.login(process.env.TOKEN);
