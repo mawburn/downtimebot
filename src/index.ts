a62c13e0413d685143c1153c48d4c6969e97653c
mawburn <mawburn7@gmail.com>
2022-04-18 18:52:47 -0400

    fix eslint

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,24 @@
 import 'dotenv/config'
-import { Client, Intents } from 'discord.js';
+import { Client, Intents } from 'discord.js'
 
-import config from './config';
+import config from './config'
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] })
 
 client.once('ready', () => {
-	console.log('Ready!');
-});
+  console.log('Ready!')
+})
 
 client.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand()) return;
+  if (!interaction.isCommand()) return
 
-	const { commandName } = interaction;
+  const { commandName } = interaction
 
-	if (commandName === 'ping') {
-		await interaction.reply('Pong!');
-	} else if (commandName === 'beep') {
-		await interaction.reply('Boop!');
-	}
-});
+  if (commandName === 'ping') {
+    await interaction.reply('Pong!')
+  } else if (commandName === 'beep') {
+    await interaction.reply('Boop!')
+  }
+})
 
-client.login(config.token);
+client.login(config.token)
